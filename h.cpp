504e02ede3b220e5b107e0d5803f35d98b38cad1
rshukla2711 <btech10189.21@bitmesra.ac.in>
2022-01-13 18:51:50 +0530

    mera pehla code

---
+++ h.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    int k;
+    int z;
+    int f;
+    int c=0;
+    int i=0;
+    cin>>n>>k;
+    while(i<=n);(i++);{
+        cin<<f;
+        if(f%k==0){
+            c++;
+        }   else{
+            c=c;
+        }
+    } cout<<c;
+    
+    
+}
